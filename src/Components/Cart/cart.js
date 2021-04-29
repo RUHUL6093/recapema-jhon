3ce36048e5406583260c03b530d8386d4dd48097
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-30 05:27:55 +0600

    go total

--- src/Components/Cart/cart.js
+++ src/Components/Cart/cart.js
@@ -1,10 +1,13 @@
 import React from "react";
 const Cart = (props) => {
   const cart = props.cart;
+  const total = cart.reduce((total, product) => total + product.price, 0);
+
   return (
     <div>
       <h1>Order summery</h1>
       <h4>Item ordered: {cart.length}</h4>
+      <p> Toatal Price :{total}</p>
     </div>
   );
 };
