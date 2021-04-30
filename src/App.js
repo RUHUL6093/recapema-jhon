ccb1ea8c56f2323352f0c869c93284ddc866a2f7
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-30 06:21:02 +0600

    404

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import Header from "./Components/Header/Header";
 import Shop from "./Components/Shop/Shop";
 import "./styles.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Review from "./Components/Review/Review";
+import Inventory from "./Components/Inventory/Inventory";
+import NotFound from "./Components/NotFound/NotFound";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Shop />
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            {" "}
+            <Shop />
+          </Route>
+          <Route path="/review">
+            {" "}
+            <Review />
+          </Route>
+          <Route path="/manage">
+            {" "}
+            <Inventory />
+          </Route>
+          <Route exact path="/">
+            {" "}
+            <Shop />
+          </Route>
+          <Route path="*">
+            {" "}
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Review/Review.js
@@ -0,0 +1,8 @@
+import React from "react";
+const  Review=()=>{
+  return (
+    <div><h1>
+      This is Review</h1></div>
+  )
+}
+export default Review;
